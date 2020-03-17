9c11944aed930ed45ee1c629ea12d8101be47433
duraimurugan.vedagiri <duraimurugna.v>
2020-03-17 22:40:58 +0530

    WF-54532 How to disable some items WinForms ComboBox dropdown

---
+++ SfComboBox/SfComboBox/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace SfComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sfComboBox1 = new Syncfusion.WinForms.ListView.SfComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.sfComboBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sfComboBox1
+            // 
+            this.sfComboBox1.Location = new System.Drawing.Point(292, 94);
+            this.sfComboBox1.Name = "sfComboBox1";
+            this.sfComboBox1.Size = new System.Drawing.Size(196, 36);
+            this.sfComboBox1.TabIndex = 0;
+            this.sfComboBox1.AllowSelectAll = true;
+            this.sfComboBox1.DisplayMember = "Name";
+            this.sfComboBox1.DataSource = this.GetData();
+            this.sfComboBox1.ComboBoxMode = Syncfusion.WinForms.ListView.Enums.ComboBoxMode.MultiSelection;
+            this.sfComboBox1.DropDownListView.DrawItem += DropDownListView_DrawItem;
+            this.sfComboBox1.DropDownListView.ItemChecking += DropDownListView_ItemChecking;
+            this.sfComboBox1.DropDownListView.SelectionChanged += DropDownListView_SelectionChanged;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sfComboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.sfComboBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Syncfusion.WinForms.ListView.SfComboBox sfComboBox1;
+    }
+}
+
---
+++ SfComboBox/SfComboBox/Form1.cs
@@ -0,0 +1,80 @@
+﻿using Syncfusion.WinForms.ListView;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SfComboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Events
+
+        int disableItemIndex = 1;
+
+        private void DropDownListView_SelectionChanged(object sender, Syncfusion.WinForms.ListView.Events.ItemSelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count == this.sfComboBox1.DropDownListView.View.Items.Count())
+            {
+                 this.sfComboBox1.DropDownListView.CheckedItems.RemoveAt(disableItemIndex - 1);
+            }
+        }
+
+        private void DropDownListView_ItemChecking(object sender, Syncfusion.WinForms.ListView.Events.ItemCheckingEventArgs e)
+        {
+            if(e.ItemIndex == disableItemIndex)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void DropDownListView_DrawItem(object sender, Syncfusion.WinForms.ListView.Events.DrawItemEventArgs e)
+        {
+            if (e.ItemIndex == disableItemIndex)
+            {
+                e.Style.BackColor = Color.LightGray;
+                e.Style.ForeColor = Color.Gray;
+            }
+        }
+
+        #endregion
+
+        #region Data Setting 
+
+        public class Details
+        {
+            public string Name { get; set; }
+        }
+
+        public List<Details> GetData()
+        {
+            List<Details> list = new List<Details>();
+            list.Add(new Details() { Name = "Amir"});
+            list.Add(new Details() { Name = "Asif"});
+            list.Add(new Details() { Name = "Catherine"});
+            list.Add(new Details() { Name = "Cindrella"});
+            list.Add(new Details() { Name = "David"});
+            list.Add(new Details() { Name = "Ellis"});
+            list.Add(new Details() { Name = "Farooq"});
+            list.Add(new Details() { Name = "Muhammad"});
+            list.Add(new Details() { Name = "Saleem"});
+            list.Add(new Details() { Name = "Usman"});
+            return list;
+        }
+
+        #endregion
+    }
+}
+
+
+
